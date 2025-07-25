f675b4041976020c830dd376516588a7a670fd6f
nicolas03 <nicolas.russo@telecomnancy.eu>
2025-07-26 00:34:49 +0200

    Add : Responsive canvas (when shown and played)

--- src/script.js
+++ src/script.js
@@ -10,6 +10,15 @@ let players = [];
 let turn = 0;
 let moves = [];
 
+constants.touchCanvas.width = (constants.NUM_TILES_X - 1) * WIDTH_TILE;
+constants.touchCanvas.height = (constants.NUM_TILES_Y - 1) * HEIGHT_TILE;
+constants.gameCanvas.width = (constants.NUM_TILES_X - 1) * WIDTH_TILE;
+constants.gameCanvas.height = (constants.NUM_TILES_Y - 1) * HEIGHT_TILE;
+constants.pathCanvas.width = (constants.NUM_TILES_X - 1) * WIDTH_TILE;
+constants.pathCanvas.height = (constants.NUM_TILES_Y - 1) * HEIGHT_TILE;
+
+const aspect = constants.touchCanvas.width / constants.touchCanvas.height; // Par exemple 16/9 ou 4/3
+updateSize();
 
 createGame()
 
@@ -19,26 +28,38 @@ function createGame() {
         const x = Math.floor(Math.random() * constants.NUM_TILES_X);
         const y = Math.floor(Math.random() * constants.NUM_TILES_Y);
         players.push(new Player(x, y,
-             `hsl(${Math.random() * 360}, 100%, 50%)`));
+            `hsl(${Math.random() * 360}, 100%, 50%)`));
     }
     // Initialisation de la grille
     fillGrid();
 
-    constants.touchCanvas.width = (constants.NUM_TILES_X - 1) * WIDTH_TILE;
-    constants.touchCanvas.height = (constants.NUM_TILES_Y - 1) * HEIGHT_TILE;
-    constants.pathCanvas.width = (constants.NUM_TILES_X - 1) * WIDTH_TILE;
-    constants.pathCanvas.height = (constants.NUM_TILES_Y - 1) * HEIGHT_TILE;
-    constants.gameCanvas.width = (constants.NUM_TILES_X - 1) * WIDTH_TILE;
-    constants.gameCanvas.height = (constants.NUM_TILES_Y - 1) * HEIGHT_TILE;
-
     initiateTurn();
+}
+
+// Add event listener for window resize
+window.addEventListener('resize', updateSize);
+
+function updateSize() {
+    const w = main.clientWidth;
+    const h = main.clientHeight;
+    if (w / h > aspect) {
+        // Trop large, on limite par la hauteur
+        constants.touchCanvas.style.height = h + 'px';
+        constants.touchCanvas.style.width = (h * aspect) + 'px';
+    } else {
+        // Trop haut, on limite par la largeur
+        constants.touchCanvas.style.width = w + 'px';
+        constants.touchCanvas.style.height = (w / aspect) + 'px';
+    }
 }
 
 constants.touchCanvas.addEventListener('click', (event) => {
     // Déterminer quelle intersection entre deux lignes a été cliquée. Genre le cadrillage, là où deux lignes se croisent.
     const rect = constants.touchCanvas.getBoundingClientRect();
-    const x = Math.floor((event.clientX - rect.left + WIDTH_TILE / 2) / WIDTH_TILE);
-    const y = Math.floor((event.clientY - rect.top + HEIGHT_TILE / 2) / HEIGHT_TILE);
+    let x = (event.clientX - rect.left + WIDTH_TILE / 2) / WIDTH_TILE;
+    x = Math.floor(x * constants.touchCanvas.width / constants.touchCanvas.offsetWidth);
+    let y = (event.clientY - rect.top + HEIGHT_TILE / 2) / HEIGHT_TILE;
+    y = Math.floor(y * constants.touchCanvas.height / constants.touchCanvas.offsetHeight);
 
     if (!players[turn].canMove(x, y)) {
         console.log(`Cannot move to (${x}, ${y})`);
@@ -46,7 +67,7 @@ constants.touchCanvas.addEventListener('click', (event) => {
     }
     players[turn].move(x, y);
     turn = (turn + 1) % numberPlayers; // Passer au joueur suivant
-    
+
     while (players[turn].stun > 0) {
         players[turn].stun--;
         turn = (turn + 1) % numberPlayers; // Passer au joueur suivant
@@ -99,11 +120,11 @@ function readSingleFileAndCreateTerrain(event) {
 }
 
 function renderCurrentGame() {
-    constants.ctxGame.clearRect(0, 0, constants.touchCanvas.width, constants.touchCanvas.height);
+    constants.ctxTouch.clearRect(0, 0, constants.touchCanvas.width, constants.touchCanvas.height);
     renderTerrain();
-    renderCanvas();
+    touchCanvas();
 
-    constants.ctxPath.clearRect(0, 0, constants.pathCanvas.width, constants.pathCanvas.height);
+    // constants.ctxTouch.clearRect(0, 0, constants.touchCanvas.width, constants.touchCanvas.height);
     for (let player of players) {
         renderPath(player);
         renderPlayer(player);
@@ -112,12 +133,12 @@ function renderCurrentGame() {
 }
 
 function renderTerrain() {
-    constants.ctxGame.clearRect(0, 0, constants.gameCanvas.width, constants.gameCanvas.height);
-    constants.ctxGame.fillStyle = '#000000';
+    // constants.ctxTouch.clearRect(0, 0, constants.touchCanvas.width, constants.touchCanvas.height);
+    constants.ctxTouch.fillStyle = '#000000';
     for (let i = 0; i < constants.NUM_TILES_X * constants.trackDensity; i++) {
         for (let j = 0; j < constants.NUM_TILES_Y * constants.trackDensity; j++) {
             if (gridElements[i][j] === 1) {
-                constants.ctxGame.fillRect(i * WIDTH_TILE / constants.trackDensity - WIDTH_TILE / (2 * constants.trackDensity),
+                constants.ctxTouch.fillRect(i * WIDTH_TILE / constants.trackDensity - WIDTH_TILE / (2 * constants.trackDensity),
                     j * HEIGHT_TILE / constants.trackDensity - HEIGHT_TILE / (2 * constants.trackDensity),
                     WIDTH_TILE / constants.trackDensity,
                     HEIGHT_TILE / constants.trackDensity);
@@ -126,50 +147,50 @@ function renderTerrain() {
     }
 }
 
-function renderCanvas() {
-    constants.ctxGame.strokeStyle = '#000000';
-    constants.ctxGame.lineWidth = 1;
+function touchCanvas() {
+    constants.ctxTouch.strokeStyle = '#000000';
+    constants.ctxTouch.lineWidth = 1;
     for (let i = 0; i <= constants.NUM_TILES_X; i++) {
-        constants.ctxGame.beginPath();
-        constants.ctxGame.moveTo(i * WIDTH_TILE, 0);
-        constants.ctxGame.lineTo(i * WIDTH_TILE, constants.touchCanvas.height);
-        constants.ctxGame.stroke();
+        constants.ctxTouch.beginPath();
+        constants.ctxTouch.moveTo(i * WIDTH_TILE, 0);
+        constants.ctxTouch.lineTo(i * WIDTH_TILE, constants.touchCanvas.height);
+        constants.ctxTouch.stroke();
     }
     for (let j = 0; j <= constants.NUM_TILES_Y; j++) {
-        constants.ctxGame.beginPath();
-        constants.ctxGame.moveTo(0, j * HEIGHT_TILE);
-        constants.ctxGame.lineTo(constants.touchCanvas.width, j * HEIGHT_TILE);
-        constants.ctxGame.stroke();
+        constants.ctxTouch.beginPath();
+        constants.ctxTouch.moveTo(0, j * HEIGHT_TILE);
+        constants.ctxTouch.lineTo(constants.touchCanvas.width, j * HEIGHT_TILE);
+        constants.ctxTouch.stroke();
     }
 }
 
 function renderPath(player) {
-    constants.ctxPath.strokeStyle = player.color;
-    constants.ctxPath.lineWidth = 3;
+    constants.ctxTouch.strokeStyle = player.color;
+    constants.ctxTouch.lineWidth = 3;
 
-    constants.ctxPath.beginPath();
-    constants.ctxPath.moveTo(player.moves[0].x * WIDTH_TILE,
+    constants.ctxTouch.beginPath();
+    constants.ctxTouch.moveTo(player.moves[0].x * WIDTH_TILE,
         player.moves[0].y * HEIGHT_TILE);
     for (let move of player.moves) {
         // Draw a line from the move position to the current player position
-        constants.ctxPath.lineTo(move.x * WIDTH_TILE,
+        constants.ctxTouch.lineTo(move.x * WIDTH_TILE,
             move.y * HEIGHT_TILE);
-        constants.ctxPath.stroke();
+        constants.ctxTouch.stroke();
     }
 }
 
 function renderPlayerMoves(player) {
-    constants.ctxPath.fillStyle = 'rgba(0, 255, 0, 0.5)';
+    constants.ctxTouch.fillStyle = 'rgba(0, 255, 0, 0.5)';
     player.possibleMoves.forEach(move => {
-        constants.ctxPath.beginPath();
-        constants.ctxPath.arc(move.x * WIDTH_TILE, move.y * HEIGHT_TILE, 10, 0, Math.PI * 2);
-        constants.ctxPath.fill();
+        constants.ctxTouch.beginPath();
+        constants.ctxTouch.arc(move.x * WIDTH_TILE, move.y * HEIGHT_TILE, 10, 0, Math.PI * 2);
+        constants.ctxTouch.fill();
     });
 }
 
 function renderPlayer(player) {
-    constants.ctxPath.fillStyle = player.color;
-    constants.ctxPath.beginPath();
-    constants.ctxPath.arc(player.position.x * WIDTH_TILE, player.position.y * HEIGHT_TILE, 10, 0, Math.PI * 2);
-    constants.ctxPath.fill();
+    constants.ctxTouch.fillStyle = player.color;
+    constants.ctxTouch.beginPath();
+    constants.ctxTouch.arc(player.position.x * WIDTH_TILE, player.position.y * HEIGHT_TILE, 10, 0, Math.PI * 2);
+    constants.ctxTouch.fill();
 }
